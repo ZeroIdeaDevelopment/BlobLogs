eff439596c98340d416d9d3acce1b040bf227304
Lewis Crichton <lewistehminerz@outlook.com>
2018-09-30 16:23:30 +0100

    remove other cancel button

--- commands.js
+++ commands.js
@@ -15,7 +15,7 @@ module.exports = (bot, loggr) => {
         fullDescription: 'A basic command just to check if the bot is online, and the latency between the bot and Discord, determined by getting the timestamp the command message was sent, creating a message, getting the timestamp of when the new one was sent, and then taking the old one from the new one (end - start).'
     });
     
-    bot.registerCommand('blob', 'Are you sure? This screws up the playing message and can take a long time if it was ratelimited previously.', {
+    bot.registerCommand('blob', 'Are you sure? This screws up the playing message and can take a long time if it was ratelimited previously. If you have changed your mind, let the reactions time out.', {
         description: 'Sets the avatar.',
         fullDescription: 'Sets the blob avatar from the blobs folder. Owner-only.',
         reactionButtons: [
@@ -23,7 +23,6 @@ module.exports = (bot, loggr) => {
                 type: 'cancel',
                 emoji: '✅',
                 async response(msg, args, userID) {
-                    if (msg.author.id !== userID) return;
                     loggr.info('Updating blob icon forcefully...');
                     let m = await msg.channel.createMessage('Updating...');
                     try {
@@ -38,11 +37,6 @@ module.exports = (bot, loggr) => {
                         await m.edit('Oh no! ' + e.message);
                     }
                 }
-            },
-            {
-                type: 'cancel',
-                emoji: '❎',
-                response: 'Cancelled.'
             }
         ],
         reactionButtonTimeout: 15000,
